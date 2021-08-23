a2729de776e00a835f04e7015125ed58d5a88e46
Àlex Llaó <allaog@hotmail.com>
2021-08-23 05:53:46 +0200

    Testeamos con más datos

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
 
+const file = require('./data/tests-data-20210823-030921.json');
+let data = [];
+
+for (let i = 0; i < file.length - 1; i++) {
+    const { created_at: val1 } = file[i];
+    const { created_at: val2 } = file[i + 1];
+
+    const date1 = new Date(val1);
+    const date2 = new Date(val2);
+        
+    const diff = Math.abs(date2 - date1);
+    data.push(diff);
+};
+
+fs.writeFileSync('./data/data-tests-createdat.json', JSON.stringify(data));
---
+++ testMiscData.js
@@ -0,0 +1,31 @@
+// Cargamos datos
+const values = require('./data/data-tests-createdat.json');
+
+// Diccionario donde dejaremos los datos
+let results = {};
+
+// Si hay valores
+if (values && values.length) {
+    for (let i = 0; i < values.length; i++) {
+
+        // Extraemos valor
+        const value = values[i];        
+
+        // Obtenemos primer dígito
+        const firstChar = value.toString()[0];
+
+        // Contamos el número de apariciones de cada primer dígito (siempre que no sea nulo)
+        if (firstChar !== "0") { results[firstChar] ? results[firstChar]++ : results[firstChar] = 1; }
+    }
+}
+
+// Calculamos el total de resultados
+const total = Object.keys(results).reduce((sum, key) => sum + parseInt(results[key] || 0), 0);
+
+// Calculamos porcentaje de la aparición de cada primer dígito
+for (const [key, value] of Object.entries(results)) {
+    results[key] = value / total * 100;
+}
+
+// Mostramos resultados
+console.log(results);
