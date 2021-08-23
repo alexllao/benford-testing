b8c1d995fc994f916a5763cefa1eb663e40d302f
Àlex Llaó <allaog@hotmail.com>
2021-08-23 03:07:16 +0200

    Simplificamos el cálculo del total

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const values = require('./data/poblacion-municipios.json');
-const { digitAt } = require('./utils.js');
 
+// Diccionario donde dejaremos los datos
 let results = {};
-let total = 0;
 
 // Si hay valores
 if (values) {
     for (let i = 0; i < values.length; i++) {
-        const element = values[i];
-        const firstDigit = digitAt(element.value, 0);
 
-        if (firstDigit != 0 && !isNaN(firstDigit)) {
+        const { value } = values[i];
+
+        // Obtenemos primer dígito
+        const firstDigit = value.toString()[0];
+
+        // Si el primer dígito no empieza por cero
+        if (firstDigit !== 0) {
 
             if (!results[firstDigit.toString()]) {
                 results[firstDigit.toString()] = 1;
@@ -22,13 +25,15 @@ if (values) {
     }
 }
 
-// Calculamos la suma
-for (const [, value] of Object.entries(results)) {
-    total += value;
-}
+// Calculamos el total de resultados
+const total = Object.keys(results).reduce((sum, key) => sum + parseFloat(results[key] || 0), 0);
 
+
+
+// Calculamos porcentaje de la aparición de cada primer dígito
 for (const [key, value] of Object.entries(results)) {
     results[key] = value / total * 100;
 }
 
-console.log(results);
+// Mostramos resultados
+console.log(total2);
